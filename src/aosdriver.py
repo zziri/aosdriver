13ccfde87b47f80c1e12ff7d5c94ef440cb4c8e3
dvlpr-shark <jihzang@naver.com>
2020-04-01 22:09:04 +0900

    bfs 최적화, 추가할 함수 프로토타입

--- src/aosdriver.py
+++ src/aosdriver.py
@@ -3,7 +3,8 @@ from ppadb.client import Client
 from xml.etree import ElementTree
 import os
 import re
-from asyncio import Queue
+from queue import Queue
+
 
 class Driver:
     def __init__(self, host='127.0.0.1', port=5037):
@@ -13,29 +14,45 @@ class Driver:
         self.SETTINGS_ACTIVITY_PATH = "com.android.settings/com.android.settings.Settings"
 
         self.devices = client.devices()
-        print('Driver@__init__() start driver')
+        print('Driver@__init__: start driver')
 
     def waitForDevice(self):
-        print('Driver@waitForDevice() waiting for devices...')
+        print('Driver@waitForDevice: waiting for devices...')
         os.popen('adb wait-for-device').read()  # read() 해줘야 대기함
 
     def getXml(self, device):
+        # path 가져와서 할 것 추가해야함
         str = device.shell('uiautomator dump')
         path = '/sdcard/window_dump.xml'
         return device.shell('cat ' + path)
 
-    def findNode(self, tree, key, value):
-        q = list()
-        q.append(tree)
-        while q:
-            cur = q.pop(0)
+    # 테스트 필요
+    def findNode(self, root, key, value):
+        q = Queue()
+        q.put(root)
+        while q.qsize():
+            cur = q.get()
             if cur.tag == 'node':
                 if cur.attrib[key] == value:
                     return cur
             nexts = cur.findall('node')
             for nxt in nexts:
-                q.append(nxt)
+                q.put(nxt)
         return None
+
+    # 위에 함수로 대체할 것임
+    # def findNode(self, tree, key, value):
+    #     q = list()
+    #     q.append(tree)
+    #     while q:
+    #         cur = q.pop(0)
+    #         if cur.tag == 'node':
+    #             if cur.attrib[key] == value:
+    #                 return cur
+    #         nexts = cur.findall('node')
+    #         for nxt in nexts:
+    #             q.append(nxt)
+    #     return None
 
     def getBoundPos(self, node):
         ret = []
@@ -73,3 +90,12 @@ class Driver:
     def wakeup(self):
         for device in self.devices:
             device.shell('input keyevent KEYCODE_WAKEUP')
+
+    def pageDownScroll(self):       # 개발 전
+        print('Driver@pageDownScroll: page down start')
+
+    def dumpstate(self):            # 개발 전
+        print('Driver@dumpstate: dumpstate start')
+
+
+
