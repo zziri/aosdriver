8fae97271fc0fd2c4ca487ffd626868b78b53f25
dvlpr-shark <jihzang@naver.com>
2020-04-18 18:32:47 +0900

    apply .gitignore

--- src/fastdumpstate.py
+++
@@ -1,9 +0,0 @@
-
-from aosdriver import *
-
-DESTINATION_PATH = "/home/jihoon/Documents/klog/fastdumpstate.lst"
-driver = Driver()
-driver.wakeup()
-paths = driver.dumpstate()
-path = paths[0]
-driver.pull(path, DESTINATION_PATH)
--- src/test.py
+++
@@ -1,4 +0,0 @@
-
-
-for a in [ i for i in tlist if i>3 ]:
-    print("안녕")
